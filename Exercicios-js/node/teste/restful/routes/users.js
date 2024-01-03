2e8c7e79587fc22c0d0acc84b1784b2600ec0e23
rafael f <enterrafael@gmail.com>
2024-01-03 13:14:04 -0300

    nedb

--- Exercicios-js/node/teste/restful/routes/users.js
+++ Exercicios-js/node/teste/restful/routes/users.js
@@ -1,21 +1,39 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
 
 module.exports = (app) =>{
 
-    app.get('/users', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users:[{
-                name: 'Hcode',
-                email: 'contato@fcide.com.br',
-                id: 1
-            }]
+    let route = app.route('/users');
+
+    route.get((req, res) => {
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+
+            if (err){
+                 app.utils.error.send(err, req, res);
+            } else{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+            }
         });
+
     });
     
-    app.post('/users', (req, res) =>{
+    route.post((req, res) =>{
 
-        res.json(req.body);
+        db.insert(req.body, (err, user)=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else{
+                res.status(200).json(user);
+            }
+        });
+
     });
-
 };
