47aa76caedc780a80a16a55fc843bfc4bbdb5c42
rafael f <enterrafael@gmail.com>
2024-01-04 17:56:09 -0300

    ajustes

--- Exercicios-js/node/teste/restful/routes/users.js
+++ Exercicios-js/node/teste/restful/routes/users.js
@@ -36,4 +36,26 @@ module.exports = (app) =>{
         });
 
     });
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) =>{
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else{
+                res.status(200).json(user);
+            }
+        });
+    });
+
+    routeId.put((req, res) =>{
+        db.update({_id:req.params.id}, req.body, err =>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else{
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        });
+    });
 };
