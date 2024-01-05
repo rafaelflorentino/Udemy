486049213af87cbc0ba3210e3a832c935bd02e22
rafael f <enterrafael@gmail.com>
2024-01-05 16:33:38 -0300

    excluir e validator

--- Exercicios-js/node/teste/restful/routes/users.js
+++ Exercicios-js/node/teste/restful/routes/users.js
@@ -27,6 +27,8 @@ module.exports = (app) =>{
     
     route.post((req, res) =>{
 
+       if(!app.utils.validator.user(app, req, res)) return false;
+
         db.insert(req.body, (err, user)=>{
             if(err){
                 app.utils.error.send(err, req, res);
@@ -50,11 +52,25 @@ module.exports = (app) =>{
     });
 
     routeId.put((req, res) =>{
+
+        if(!app.utils.validator.user(app, req, res)) return false;
+
         db.update({_id:req.params.id}, req.body, err =>{
             if(err){
                 app.utils.error.send(err, req, res);
             } else{
                 res.status(200).json(Object.assign(req.params, req.body));
+            }
+        });
+    });
+
+    routeId.delete((req, res) =>{
+
+        db.remove({_id: req.params.id}, {}, err=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else{
+                res.status(200).json(req.params);
             }
         });
     });
